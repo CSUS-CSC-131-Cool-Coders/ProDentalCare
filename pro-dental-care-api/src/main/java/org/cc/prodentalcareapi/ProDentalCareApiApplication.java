b7574b63615f680b7282ab1766ccdb9583052a4b
Caleb Brock <caleb.brock@broshin.com>
2024-10-08 10:50:23 -0700

    Initial commit with pro-dental-care-api project

---
+++ pro-dental-care-api/src/main/java/org/cc/prodentalcareapi/ProDentalCareApiApplication.java
@@ -0,0 +1,13 @@
+package org.cc.prodentalcareapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProDentalCareApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProDentalCareApiApplication.class, args);
+	}
+
+}
---
+++ pro-dental-care-api/src/test/java/org/cc/prodentalcareapi/ProDentalCareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cc.prodentalcareapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProDentalCareApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
