cdda3ba87f5e69b8edf07f4218b3cc34cda46d6a
Caleb Brock <caleb.brock@broshin.com>
2024-10-29 15:44:16 -0700

    DT-30 - Implement Patient class and Patient Repository

---
+++ pro-dental-care-api/src/main/java/org/cc/prodentalcareapi/model/Patient.java
@@ -0,0 +1,132 @@
+package org.cc.prodentalcareapi.model;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity()
+public class Patient {
+
+	@Id
+	@Column(name = "patient_id", length = 9, columnDefinition = "char(9)")
+	private String patientId;
+
+	@Column(name = "email", length = 100)
+	private String email;
+
+	@Column(name = "pass_hash", length = 128)
+	private String passHash;
+
+	@Column(name = "fname", length = 40)
+	private String firstName;
+
+	@Column(name = "lname", length = 40)
+	private String lastName;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name = "dob")
+	private Date dateOfBirth;
+
+	@Column(name = "phone_no", length = 11)
+	private String phoneNumber;
+
+	@Column(name = "sex", length = 40)
+	private String sex;
+
+	@Column(name = "lang_code", length = 2, columnDefinition = "char(2)")
+	private String langCode;
+
+	@Column(name = "weight")
+	private int weight;
+
+	@Column(name = "height")
+	private int height;
+
+	public String getPatientId() {
+		return patientId;
+	}
+
+	public void setPatientId(String patientId) {
+		this.patientId = patientId;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassHash() {
+		return passHash;
+	}
+
+	public void setPassHash(String passHash) {
+		this.passHash = passHash;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getLangCode() {
+		return langCode;
+	}
+
+	public void setLangCode(String langCode) {
+		this.langCode = langCode;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+}
