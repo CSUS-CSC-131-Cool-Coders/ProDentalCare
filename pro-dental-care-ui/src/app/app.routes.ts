22b2475da139c928d198a9e4ad91b9b24f0bd46b
anha-cs <anha@csus.edu>
2024-11-07 20:45:16 -0800

    Updated the app routes file.

--- pro-dental-care-ui/src/app/app.routes.ts
+++ pro-dental-care-ui/src/app/app.routes.ts
@@ -3,12 +3,14 @@ import {LoginPageComponent} from "./account/login-page/login-page.component";
 import {ForgotPasswordComponent} from "./account/forgot-password/forgot-password.component";
 import {SignupPageComponent} from "./account/signup-page/signup-page.component";
 import {ResetPasswordComponent} from "./account/reset-password/reset-password.component";
+import {VisitorDashboardComponent} from './dashboard/visitor-dashboard/visitor-dashboard.component';
 
 export const routes: Routes = [
     {path: "login", component: LoginPageComponent},
     {path: "signup", component: SignupPageComponent},
     {path: "forgot-password", component: ForgotPasswordComponent},
     {path: "reset-password", component: LoginPageComponent},
-    {path: "reset-password/:resetToken", component: ResetPasswordComponent}
+    {path: "reset-password/:resetToken", component: ResetPasswordComponent},
+    {path: "visitor-dashboard", component: VisitorDashboardComponent}
 
 ];
