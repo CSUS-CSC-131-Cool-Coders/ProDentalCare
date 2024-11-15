25d8a5c1addd75dfeefdd06b1d49a15af618b061
Nick Gomes <cricketclan27@gmail.com>
2024-11-14 21:31:15 -0800

    Review API Class

---
+++ pro-dental-care-api/src/main/java/org/cc/prodentalcareapi/model/Review.java
@@ -0,0 +1,52 @@
+package org.cc.prodentalcareapi.model;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+//apointment and patient id needed, makes a review id, then makes a review
+
+@Entity
+@Table(name = "reviews")
+public class review
+{
+    @Id
+    @Column(name = "patient_id_fk", length = 9)
+    private String patient_id_fk;
+
+    @Column(name = "appt_id_fk")
+    private int appt_id_fk;
+    
+    @Column(name = "contents", length = 200)
+    private String contents;
+    
+    public void setPatientId(String id)
+    {
+        patient_id_fk = id;
+    }
+
+    public void setAppt_ID(int id)
+    {
+        appt_ID = id;
+    }
+
+    public void setContents(String contents)
+    {
+        this.contents = contents;
+    }
+
+    public String getPatientId()
+    {
+        return patient_id_fk;
+    }
+
+    public int getAppt_ID()
+    {
+        return appt_id_fk;
+    }
+
+    public String getContents()
+    {
+        return contents;
+    }
+}
