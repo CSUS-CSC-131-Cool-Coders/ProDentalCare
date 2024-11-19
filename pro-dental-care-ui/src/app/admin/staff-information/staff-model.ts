3234ca0cc5960fc01b63f178a777977cb7ee3318
brentcrosby <heybrentcrosby@gmail.com>
2024-11-19 13:14:39 -0800

    Add admin view staff info page

---
+++ pro-dental-care-ui/src/app/admin/staff-information/staff-model.ts
@@ -0,0 +1,11 @@
+export interface Staff {
+  id: number;
+  name: string;
+  position: string;
+  pay: string;
+  yearsWorked: number;
+  email: string;
+  contactNumber: string;
+  qualifications: string[];
+  profilePic?: string;
+}
