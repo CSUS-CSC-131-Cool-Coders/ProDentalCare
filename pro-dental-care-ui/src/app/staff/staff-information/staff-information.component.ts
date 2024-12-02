001d260ca7a0bca9e1f33b5d64f5a7953dd4764b
seantillman <seantillman4@gmail.com>
2024-12-02 02:48:34 -0800

    Staff info page UI

---
+++ pro-dental-care-ui/src/app/staff/staff-information/staff-information.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-staff-information',
+  standalone: true,
+  imports: [],
+  templateUrl: './staff-information.component.html',
+  styleUrl: './staff-information.component.css'
+})
+export class StaffInformationComponent {
+
+}
