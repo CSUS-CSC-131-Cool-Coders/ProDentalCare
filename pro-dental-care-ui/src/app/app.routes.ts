5e9038274412852ed4b59f98379aa66885e80098
anha-cs <anha@csus.edu>
2024-12-02 16:11:53 -0800

    Changed the health-records import path for api Changed the app routes

--- pro-dental-care-ui/src/app/app.routes.ts
+++ pro-dental-care-ui/src/app/app.routes.ts
@@ -1,39 +1,36 @@
-import {Routes} from '@angular/router';
-import {LoginPageComponent} from "./account/login-page/login-page.component";
-import {ForgotPasswordComponent} from "./account/forgot-password/forgot-password.component";
-import {SignupPageComponent} from "./account/signup-page/signup-page.component";
-import {ResetPasswordComponent} from "./account/reset-password/reset-password.component";
-import {PaymentComponent} from "./patient/payment/payment.component";
-import {PaymentPageComponent} from "./patient/payment-processing/payment-processing.component";
-import {VisitorDashboardComponent} from './dashboard/visitor-dashboard/visitor-dashboard.component';
-import {Page2Component} from "./account/signup-page/page-2/page-2.component";
-import {PatientOverviewComponent} from './patient/overview/patient-overview.component';
-import {PatientHealthRecords} from './patient/records/health records/health-records.component';
-import {PatientTreatmentPlan} from './patient/records/treatment plan/patient-treatment-plan.component';
-import {StaffPatientInfoComponent} from './src/app/staff-patient-info/staff-patient-info.component';
-import {StaffPatientInfoPage2Component} from './src/app/staff-patient-info/staff-patient-info-page2/staff-patient-info-page2.component';
-import {PatientInformationComponent} from './patient/patient-information/patient-information.component';
-import {StaffSignupComponent} from "./account/signup-page/staff-signup/staff-signup.component";
-import {StaffInformationComponent} from "./staff-staff info/staff-information.component";
+import { Routes } from '@angular/router';
+import { LoginPageComponent } from "./account/login-page/login-page.component";
+import { ForgotPasswordComponent } from "./account/forgot-password/forgot-password.component";
+import { SignupPageComponent } from "./account/signup-page/signup-page.component";
+import { ResetPasswordComponent } from "./account/reset-password/reset-password.component";
+import { PaymentComponent } from "./patient/payment/payment.component";
+import { PaymentPageComponent } from "./patient/payment-processing/payment-processing.component";
+import { VisitorDashboardComponent } from './dashboard/visitor-dashboard/visitor-dashboard.component';
+import { Page2Component } from "./account/signup-page/page-2/page-2.component";
+import { PatientOverviewComponent } from './patient/overview/patient-overview.component';
+import { PatientHealthRecords } from './patient/records/health records/health-records.component';
+import { PatientTreatmentPlan } from './patient/records/treatment plan/patient-treatment-plan.component';
+import { PatientInformationComponent } from './patient/patient-information/patient-information.component';
+import { StaffSignupComponent } from "./account/signup-page/staff-signup/staff-signup.component";
+import { StaffInformationComponent } from "./staff-staff info/staff-information.component";
 
 export const routes: Routes = [
-    {path: "login", component: LoginPageComponent},
-    {path: "signup", component: SignupPageComponent},
-    {path: "signup/page-2", component: Page2Component},
-    {path: "staff/signup", component: StaffSignupComponent},
-    {path: "forgot-password", component: ForgotPasswordComponent},
-    {path: "reset-password", component: LoginPageComponent},
-    {path: "reset-password/:resetToken", component: ResetPasswordComponent},
-    {path: "dashboard", component: VisitorDashboardComponent},
-    {path: "patient/dashboard", component: PatientOverviewComponent}, // Add the route for OverviewComponent
-    {path: "patient/records", component: PatientHealthRecords},
-    {path: "patient/treatment", component: PatientTreatmentPlan},
-    {path: "patient/patient-information", component: PatientInformationComponent},
-    {path: "reset-password/:resetToken", component: ResetPasswordComponent},
-    {path: "payments", component: PaymentComponent}, //Add route for payments
-    {path: "process-payment", component: PaymentPageComponent}, //Add route for payment process
-    {path: "staff/staff-information", component: StaffInformationComponent}
-    {path: "patient/information", component: PatientInformationComponent},
-    {path: "patient/payments", component: PaymentComponent}, //Add route for payments
-    {path: "patient/process-payment", component: PaymentPageComponent} //Add route for payment process
+  { path: "login", component: LoginPageComponent },
+  { path: "signup", component: SignupPageComponent },
+  { path: "signup/page-2", component: Page2Component },
+  { path: "staff/signup", component: StaffSignupComponent },
+  { path: "forgot-password", component: ForgotPasswordComponent },
+  { path: "reset-password", component: LoginPageComponent },
+  { path: "reset-password/:resetToken", component: ResetPasswordComponent },
+  { path: "dashboard", component: VisitorDashboardComponent },
+  { path: "patient/dashboard", component: PatientOverviewComponent },
+  { path: "patient/records", component: PatientHealthRecords },
+  { path: "patient/treatment", component: PatientTreatmentPlan },
+  { path: "patient/patient-information", component: PatientInformationComponent },
+  { path: "payments", component: PaymentComponent },
+  { path: "process-payment", component: PaymentPageComponent },
+  { path: "staff/staff-information", component: StaffInformationComponent },
+  { path: "patient/information", component: PatientInformationComponent },
+  { path: "patient/payments", component: PaymentComponent },
+  { path: "patient/process-payment", component: PaymentPageComponent }
 ];
